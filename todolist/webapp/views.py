8d1922cfdf90c0fd99db2204e4401004ea823ae9
TairovCh <tairovchsh@gmail.com>
2022-11-07 22:53:12 +0600

    add list tasks

--- todolist/webapp/views.py
+++ todolist/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from webapp.models import Task
 # Create your views here.
+
+
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {"tasks":tasks})
