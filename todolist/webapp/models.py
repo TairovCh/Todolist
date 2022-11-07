4cc77f11234809433fdba033e028ec5da1aecf42
TairovCh <tairovchsh@gmail.com>
2022-11-07 21:52:13 +0600

    change migrate & add admin.py class TaskAdmin

--- todolist/webapp/models.py
+++ todolist/webapp/models.py
@@ -5,7 +5,7 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=60, verbose_name='Название')
-    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='стфтус')
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Cтaтус')
     deadline = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
 
     def __str__(self):
