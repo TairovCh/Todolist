92b93935b702f9282638d8a90b81589b0cbcebb6
TairovCh <tairovchsh@gmail.com>
2022-11-08 00:12:21 +0600

    add create new task

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import index_view, create_task
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view),
+    path('create/', create_task),
+
 ]
