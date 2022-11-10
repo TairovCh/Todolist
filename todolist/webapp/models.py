03c1b598d06b0a3892ca9153646fdb2f8aea46e5
TairovCh <tairovchsh@gmail.com>
2022-11-11 03:25:40 +0600

    added new field to model.py and migrated

--- todolist/webapp/models.py
+++ todolist/webapp/models.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     title = models.CharField(max_length=60, verbose_name='Название')
     status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Cтaтус')
     deadline = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание")
 
     def __str__(self):
         return f"{self.id}. {self.title}"
