330112f9d822350b31474e371e816290e346f1bf
TairovCh <tairovchsh@gmail.com>
2022-11-11 16:46:11 +0600

    add task_update. errors remane status in task_update.html & date format

--- todolist/webapp/views.py
+++ todolist/webapp/views.py
@@ -32,3 +32,15 @@ def create_task(request):
             deadline = None
         new_task = Task.objects.create(title=title, status=status, deadline=deadline, description=description)
         return redirect('task_view', pk=new_task.pk)
+
+def task_update_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "GET":
+        return render(request, 'task_update.html', {'task': task})
+    elif request.method == "POST":
+        task.title = request.POST.get('title')
+        task.status = request.POST.get('status')
+        task.deadline = request.POST.get('deadline')
+        task.description = request.POST.get('description') 
+        task.save()   
+        return redirect("task_view", pk=task.pk)   
