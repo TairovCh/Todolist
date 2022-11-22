792063c636635f430cc653925094729be597f7b4
TairovCh <tairovchsh@gmail.com>
2022-11-22 15:03:03 +0600

    in models.py changed type_task manytomanyfield

--- todolist/webapp/admin.py
+++ todolist/webapp/admin.py
@@ -3,7 +3,7 @@ from webapp.models import Task, StatusTask, TypeTask
 
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'description', 'status', 'type_task', "create_time", 'update_time']
+    list_display = ['id', 'summary', 'description', 'status', "create_time", 'update_time']
     list_filter = ['status']
     search_fields = ['summary']
     exclude = []
--- todolist/webapp/models.py
+++ todolist/webapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class TypeTask(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
 
     def __str__(self):
         return f"{self.pk}. {self.title}"
+
 
 class StatusTask(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
@@ -12,11 +14,12 @@ class StatusTask(models.Model):
     def __str__(self):
         return f"{self.pk}. {self.title}"
 
+
 class Task(models.Model):
     summary = models.CharField(max_length=60, verbose_name='Краткое описание')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Полное описание")
     status = models.ForeignKey('webapp.StatusTask', on_delete=models.PROTECT, related_name='tasks', verbose_name='Статус')
-    type_task = models.ForeignKey('webapp.TypeTask', on_delete=models.PROTECT, related_name='tasks', verbose_name='Тип')
+    type_task = models.ManyToManyField('webapp.TypeTask', related_name='tasks', blank=True)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     update_time = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
 
