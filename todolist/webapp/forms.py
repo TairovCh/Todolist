41bf675e3003f942e7e9b8ef401d640ea3edc074
TairovCh <tairovchsh@gmail.com>
2022-11-25 17:36:08 +0600

    update forms.py

--- todolist/webapp/forms.py
+++ todolist/webapp/forms.py
@@ -13,21 +13,31 @@ class TaskForm(forms.ModelForm):
 
     def clean_summary(self):
         summary = self.cleaned_data['summary']
+        bans = ['test_1', 'test_2', "test_3"]
         if len(summary) < 5:
             self.add_error('summary', ValidationError('Длина поля должна составлять не меенее %(length)d символов!', code='too_short', params={'length': 5}))
         elif len(summary) > 50:
             self.add_error('summary', ValidationError('Длина поля должна составлять не более %(length)d символов!', code='too_long', params={'length': 50}))
+        for ban in bans:
+            if summary == ban:
+                self.add_error('summary', ValidationError("Эти слова запрешено вводить!"))
         return summary
+
 
     def clean_description(self):
         description = self.cleaned_data['description']
+        bans = ['test_1', 'test_2', "test_3"]
+        for ban in bans:
+            for desc in description.split():
+                if desc == ban:
+                    self.add_error('summary', ValidationError("Эти слова запрешено вводить!"))
         if len(description) < 30:
             self.add_error('description', ValidationError('Длина поля должна составлять не меенее %(length)d символов!', code='too_short', params={'length': 30}))
         return description
 
 
-    def clean(self):
-        cleaned_data = super().clean()
-        if cleaned_data['summary'] == cleaned_data.get('description', ''):
-            raise ValidationError('Краткое описание и полное описание не должны быть одинаковыми')
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     if cleaned_data['summary'] == cleaned_data.get('description', ''):
+    #         raise ValidationError('Краткое описание и полное описание не должны быть одинаковыми')
+    #     return cleaned_data
