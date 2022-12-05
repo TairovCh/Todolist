e1eead1acc1e6e9732b30120c76792e7aaf0e031
TairovCh <tairovchsh@gmail.com>
2022-12-06 04:57:53 +0600

    save models.py & admin.py

--- todolist/webapp/admin.py
+++ todolist/webapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from webapp.models import Task, StatusTask, TypeTask
+from webapp.models import Task, StatusTask, TypeTask, ProjectTask
 
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'description', 'status', "create_time", 'update_time']
+    list_display = ['id', 'summary', 'description', 'status', "create_time", 'update_time', 'project_task']
     list_filter = ['status']
     search_fields = ['summary']
     exclude = []
@@ -12,11 +12,17 @@ class TaskAdmin(admin.ModelAdmin):
 admin.site.register(Task, TaskAdmin)
 
 class StatusTaskAdmin(admin.ModelAdmin):
-    list_display: ['title']
+    list_display = ['title']
 
 admin.site.register(StatusTask, StatusTaskAdmin)
 
 class TypeTaskAdmin(admin.ModelAdmin):
-    list_display: ['title']
+    list_display = ['title']
 
 admin.site.register(TypeTask, TypeTaskAdmin)
+
+
+class ProjectTaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'start_date', 'end_date']
+
+admin.site.register(ProjectTask, ProjectTaskAdmin)
--- todolist/webapp/models.py
+++ todolist/webapp/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
+
+class ProjectTask(models.Model):
+    name = models.CharField(max_length=25, verbose_name = 'Название')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    start_date = models.DateField(default=timezone.now, verbose_name='Дата начала')
+    end_date = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
 
 class TypeTask(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
@@ -18,6 +28,9 @@ class StatusTask(models.Model):
 class Task(models.Model):
     summary = models.CharField(max_length=60, verbose_name='Краткое описание')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Полное описание")
+
+    project_task = models.ForeignKey("webapp.ProjectTask", verbose_name=("Проект"), related_name='tasks', on_delete=models.PROTECT)
+
     status = models.ForeignKey('webapp.StatusTask', on_delete=models.PROTECT, related_name='tasks', verbose_name='Статус')
     type_task = models.ManyToManyField('webapp.TypeTask', related_name='tasks', blank=True)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
