f8ccdf22f556779166dbb050753172518e0be993
TairovCh <tairovchsh@gmail.com>
2022-12-20 05:02:15 +0600

    For registration we use generic CreateView

--- todolist/accounts/views.py
+++ todolist/accounts/views.py
@@ -1,33 +1,59 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from accounts.forms import MyUserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your views here.
-def register_view(request, *args, **kwargs):
-    form = MyUserCreationForm()
-    if request.method == "POST":
-        form = MyUserCreationForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('webapp:index')
-    return render(request, 'user_registration.html', {'form': form})
+
+class RegisterView(CreateView):
+    model = User
+    form_class = MyUserCreationForm
+    template_name = 'user_registration.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+
+        next_url = self.request.POST.get('next')
+        if next_url:
+            return next_url
+
+        return reverse('webapp:index')
 
 
-def login_view(request):
-    context = {}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('webapp:index')
-        else:
-            context['has_error'] = True
-    return render(request, 'login.html', context=context)
+# def register_view(request, *args, **kwargs):
+#     form = MyUserCreationForm()
+#     if request.method == "POST":
+#         form = MyUserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('webapp:index')
+#     return render(request, 'user_registration.html', {'form': form})
 
 
-def logout_view(request):
-    logout(request)
-    return redirect('webapp:index')
+# def login_view(request):
+#     context = {}
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('webapp:index')
+#         else:
+#             context['has_error'] = True
+#     return render(request, 'login.html', context=context)
+
+
+# def logout_view(request):
+#     logout(request)
+#     return redirect('webapp:index')
