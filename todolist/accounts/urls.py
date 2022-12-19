f4b5d0587775f8959e73689133e1d24bcebd4832
TairovCh <tairovchsh@gmail.com>
2022-12-20 02:50:04 +0600

    Registration. Normal modal form, password validation with password confirmation and password encryption in the database.

--- todolist/accounts/urls.py
+++ todolist/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import login_view, logout_view
+from accounts.views import register_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
@@ -7,5 +7,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', register_view, name='registration'),
 ]
--- todolist/accounts/views.py
+++ todolist/accounts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from accounts.forms import MyUserCreationForm
 
 # Create your views here.
+def register_view(request, *args, **kwargs):
+    form = MyUserCreationForm()
+    if request.method == "POST":
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    return render(request, 'user_registration.html', {'form': form})
+
+
 def login_view(request):
     context = {}
     if request.method == 'POST':
