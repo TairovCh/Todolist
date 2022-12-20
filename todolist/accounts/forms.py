5a4cb76492ed1f2161c7f5c5a4476d46a487b11d
TairovCh <tairovchsh@gmail.com>
2022-12-20 18:16:53 +0600

    Corrections in validation

--- todolist/accounts/forms.py
+++ todolist/accounts/forms.py
@@ -7,7 +7,7 @@ class MyUserCreationForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data['email']
         if email == '':
-            self.add_error('email', ValidationError('Почта обязательно к заполнению', code='null'))
+            self.add_error('email', ValidationError('Почта обязательно к заполнению', code='error_email'))
         return email
 
 
@@ -17,7 +17,7 @@ class MyUserCreationForm(UserCreationForm):
         last_name = cleaned_data.get("last_name")
         if first_name == '':
             if last_name == '':
-                raise forms.ValidationError('Заполните Имя или Фамилию!')
+                raise forms.ValidationError('Заполните Имя или Фамилию!', code='error_first_last_name')
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
