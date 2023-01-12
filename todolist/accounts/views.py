11a39caa36ee98617db3d63d28d32bbb63be4973
TairovCh <tairovchsh@gmail.com>
2023-01-12 14:19:34 +0600

    A user's personal page has been created, where you can display his personal data and data from the profile. Also output a list of projects the user is involved in with links to them.

--- todolist/accounts/views.py
+++ todolist/accounts/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import redirect
 from django.contrib.auth import login
 from accounts.forms import MyUserCreationForm
-from django.views.generic import CreateView
-from django.contrib.auth.models import User
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse
+from accounts.models import Profile
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import MultipleObjectMixin
 
 # Create your views here.
 
 class RegisterView(CreateView):
-    model = User
+    model = get_user_model()
     form_class = MyUserCreationForm
     template_name = 'user_registration.html'
 
     def form_valid(self, form):
         user = form.save()
+        Profile.objects.create(user=user)
         login(self.request, user)
         return redirect(self.get_success_url())
 
@@ -25,5 +29,18 @@ class RegisterView(CreateView):
         if next_url:
             return next_url
         return reverse('webapp:index')
+
+
+class UserDetailView(LoginRequiredMixin, DetailView, MultipleObjectMixin):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        projects = self.get_object().projecttasks.all()
+        return super().get_context_data(object_list=projects, **kwargs)
+
+
 
 
